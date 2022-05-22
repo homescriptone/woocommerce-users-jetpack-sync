46e03e3196dced71a4e98dd89126c574ddc07466
Emmanuel A <emmanueladekplovi1@gmail.com>
2022-05-23 00:22:41 +0100

    Initialise project file

---
+++ woo_ujc.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * @wordpress-plugin
+ * Plugin Name:       Sync WooCommerce Users with JetPack CRM
+ * Plugin URI:        https://dev.homescriptone.com
+ * Description:       Sync WooCommerce Users with JetPack CRM
+ * Version:           1.0.0
+ * Author:            HomeScript
+ * Author URI:        https://dev.homescriptone.com
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       woo_ujc
+ * Domain Path:       /languages
+ */
+
+define('JETPACK_CRM_API_KEY', 'Put your jetpack crm api key');
+define('JETPACK_CRM_API_SECRET', 'Put your jetpack crm api secret key');
+
+
+add_action("woocommerce_created_customer",'woo_ujc_add_new_users_to_sys' );
+
+
+function woo_ujc_add_new_users_to_sys( $customer_id ){
+    $wc_customer   = new WC_Customer( $customer_id );
+    
+    $data = array(
+		'status' => 'Lead',
+		'fname'  => 'Put your full name',
+		'email'  => 'Put your email address',
+		'country'  => 'Africa-BJ',
+	);
+
+	$curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => get_site_url().'/zbs_api/create_customer?api_key='.JETPACK_CRM_API_KEY.'&api_secret='. JETPACK_CRM_API_SECRET,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => wp_json_encode( $data ),
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json'
+    ),
+    ));
+
+    curl_exec($curl);
+
+    curl_close($curl);
+    
+}
